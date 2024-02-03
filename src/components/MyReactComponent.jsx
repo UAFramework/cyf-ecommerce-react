0f6374c8f2862fcfe294641d559439349b3bb2ae
iamoksanao200 <oksanao200@gmail.com>
2024-02-03 13:45:22 +0200

    level 1 completed

--- src/components/MyReactComponent.jsx
+++ src/components/MyReactComponent.jsx
@@ -1,24 +1,44 @@
 import './MyReactComponent.css'
-import products from '../data/Products.jsx'
 
-export default function MyReactComponent() {
 
-  const words = ["one", "Two", "Three", "Four", "Five"];
+export default function MyReactComponent({ products, products_availability }) {
 
   return (
-    <>
-      <div id="words">{words.join(", ")}</div>
-      <div id="products">
-        <h2>Products:</h2>
-        <ul>
-          {
-            products.map(product =>
-              <li key={product.id}>{product.product_name}</li>
-            )
-          }
-        </ul>
-      </div>
-    </>
-  )
+    <table className='ProductsTable'>
+      <tbody>
+        <tr>
+          <th>
+            <h3>Product</h3>
+          </th>
+          <th>
+            <h3>Price in store</h3>
+          </th>
+        </tr>
+        {products.map(product => {
+          return (
+            <tr key={product.id}>
+              <td>{product.product_name}</td>
+              <td>
+                {
+                  /* {products_availability.unit_price} */
+                  // products_availability[0].unit_price
 
+                  products_availability.filter(product_availability => product_availability.product_id === product.id)
+                    .map(product_availability => {
+
+                      return (
+                        <p key={product_availability.supplier_id}>${product_availability.unit_price} at {product_availability.supplier_name}</p>
+                      )
+                      // product_availability.unit_price, product_availability.supplier_name
+                      // unit_price, supplier_name
+                    })
+                }
+
+              </td>
+
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>)
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'   // import React component from react module
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx' // import a App component from ./App.jsx module
-import './index.css'        // import styles
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root'))
-.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
