28d9d37eeb9c9118b5a43714e50569fbe89f7c03
Oleksandr <aleksandr.savelyev@gmail.com>
2024-02-11 22:33:27 +0200

    remove unused files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import MyReactComponent from './components/MyReactComponent.jsx'
 import './App.css'
 
--- client/src/components/MyReactComponent.jsx
+++ client/src/components/MyReactComponent.jsx
@@ -1,4 +1,3 @@
-// MyReactComponent.jsx
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './MyReactComponent.css';
