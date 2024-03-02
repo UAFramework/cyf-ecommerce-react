dc7a7878ee7a22a88a065648b008302e0e9816c3
iamoksanao200 <oksanao200@gmail.com>
2024-03-02 15:32:31 +0200

    check runing details

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
-import MyReactComponent from './components/MyReactComponent.jsx'
 import products from './data/Products.jsx'
-import products_availability from './data/ProductsAvailability.jsx'
+import Product from "./components/Product.jsx"
 import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
 
 function App() {
- return(
-<>
-  <MyReactComponent products = { products }  products_availability = { products_availability } />    
-</>
- ) 
+  return (
+    <>
+     <h2>Products</h2>
+     <ul>
+      {
+        products.map(product =>
+          <li key={product.id}><Product productDetails={product} /></li>
+        )
+      }
+      
+     </ul>
+      
+    </>
+  )
 }
 
 export default App
